77a5225b52e1c0af6357785e5fc183e8c7161ef8
Oliver Boudet <oboudet1@me.com>
2018-12-18 09:35:31 +0100

    button

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,9 @@
 const SearchBar = new mdc.textField.MDCTextField(
   document.querySelector(".mdc-text-field")
 );
+const SearchButton = new mdc.ripple.MDCRipple(
+  document.querySelector(".mdc-button")
+);
 
 // Debugging.
 window.mdc = mdc;
