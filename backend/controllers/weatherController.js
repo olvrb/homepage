582e63ab0ddec6dc78e6bc58d77ad5dd774d3043
Oliver Boudet <oboudet1@me.com>
2019-01-21 09:17:24 +0100

    fix stuff, move to ddg

--- backend/controllers/weatherController.js
+++ backend/controllers/weatherController.js
@@ -1,18 +1,20 @@
-const {
-    Configuration
-} = require("../../config");
+const { Configuration } = require("../../config");
 const DarkSky = require("dark-sky");
 const darksky = new DarkSky(Configuration.Weather.SecretKey);
 
 module.exports.weatherController = async (req, res, next) => {
-    if (req.get("host") !== "homepage.olivrb.xyz" && req.get("host") !== "localhost:3220") return res.status(403).end();
-    darksky
-        .latitude(req.body.lat)
-        .longitude(req.body.long)
-        .units("auto")
-        .language("en")
-        .get()
-        .then((e) => {
-            res.json(e);
-        });
+  if (
+    req.get("host") !== "homepage.olivrb.xyz" &&
+    req.get("host") !== "localhost:3220"
+  )
+    return res.status(403).end();
+  darksky
+    .latitude(req.body.lat)
+    .longitude(req.body.long)
+    .units("auto")
+    .language("en")
+    .get()
+    .then(e => {
+      res.json(e);
+    });
 };
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,45 +1,47 @@
 const SearchTextField = new mdc.textField.MDCTextField(
-    document.querySelector(".mdc-text-field")
+  document.querySelector(".mdc-text-field")
 );
 const SearchButton = new mdc.ripple.MDCRipple(
-    document.querySelector(".mdc-button")
+  document.querySelector(".mdc-button")
 );
 
 // Debugging.
 window.mdc = mdc;
 
-document.querySelector("#google").onsubmit = (e) => {
-    console.log("s u b m i t");
-    window.location = `https://google.com/search?q=${encodeURIComponent(
-        SearchTextField.value
-    )}`;
-    return false;
+document.querySelector("#google").onsubmit = e => {
+  console.log("s u b m i t");
+  window.location = `https://duckduckgo.com/?q=${encodeURIComponent(
+    SearchTextField.value
+  )}`;
+  return false;
 };
 
 (() => {
-    SearchTextField.focus();
-    if (!navigator.geolocation) return;
-    navigator.geolocation.getCurrentPosition(DisplayLocation);
+  SearchTextField.focus();
+  if (!navigator.geolocation) return;
+  navigator.geolocation.getCurrentPosition(DisplayLocation);
 })();
 
 async function DisplayLocation(pos) {
-    const desc = document.getElementById("weatherDesc")
-    const skycons = new Skycons({
-        "color": "#eee"
-    });
+  const desc = document.getElementById("weatherDesc");
+  const skycons = new Skycons({
+    color: "#eee"
+  });
 
-    const forecast = await (await fetch("/api/v1/weather", {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            lat: pos.coords.latitude,
-            long: pos.coords.longitude
-        })
-    })).json();
-    skycons.add("weathericon", forecast.currently.icon)
-    skycons.play();
-    desc.innerHTML = `${Math.floor(forecast.currently.temperature)}&deg;C, ${forecast.currently.summary}`;
+  const forecast = await (await fetch("/api/v1/weather", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      lat: pos.coords.latitude,
+      long: pos.coords.longitude
+    })
+  })).json();
+  skycons.add("weathericon", forecast.currently.icon);
+  skycons.play();
+  desc.innerHTML = `${Math.floor(forecast.currently.temperature)}&deg;C, ${
+    forecast.currently.summary
+  }`;
 }
